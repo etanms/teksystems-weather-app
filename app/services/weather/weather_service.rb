8815f3ec2d9ca8a1a7e6fe7fcbf6b2e36275727c
etanms <etanms@gmail.com>
2025-08-23 08:51:22 -0500

    Added service for retrieving the weather forecast based on a given location.

---
+++ app/services/weather/weather_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Weather
+  module WeatherService
+    def self.get_forecast(args)
+      # Retrieve data about the location specified by the lat/lon within args
+      response   = NationalWeatherService.send_request(NationalWeatherService::Endpoints::GET_LOCATION_DATA, args)
+      properties = response.body.as_hash['properties']
+      
+      # Use the retrieved data to retrieve the forecast for this location
+      location_args = { grid_id: properties['gridId'], x: properties['gridX'], y: properties['gridY'] }
+      response      = NationalWeatherService.send_request(
+                        NationalWeatherService::Endpoints::GET_FORECAST,
+                        location_args
+                      )
+      
+      # Return forecast data
+      response.body.as_hash['properties']['periods']
+    end
+  end
+end
