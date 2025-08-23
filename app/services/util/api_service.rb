56ec86e98dc71e098f3b02d11764de38292a160a
etanms <etanms@gmail.com>
2025-08-22 22:52:20 -0500

    Added generic service for interacting with APIs.

---
+++ app/services/util/api_service.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Util
+  module ApiService
+    def send_request(endpoint, args = {})
+      data = send(endpoint, args)
+      Util::Http::HttpService.send_request(**build_arguments(data))
+    end
+    
+    private
+    
+    def build_arguments(args)
+      {
+        method:  args[:method] || Util::Http::HttpService::RequestMethods::GET,
+        options: {
+          body:    args[:body].present? ? JSON.generate(args[:body]) : nil,
+          headers: JSON.parse((self::HEADERS || {}).to_json),
+          query:   args[:query]
+        },
+        url:     self::BASE_URL + args[:path]
+      }
+    end
+  end
+end
