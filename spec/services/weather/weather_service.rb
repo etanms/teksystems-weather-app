8383ca278187af713f7527021c95ece6929b945b
etanms <etanms@gmail.com>
2025-08-23 09:48:23 -0500

    Added unit testing for weather_service.rb.

---
+++ spec/services/weather/weather_service.rb
@@ -0,0 +1,63 @@
+require "rails_helper"
+
+RSpec.describe Weather::WeatherService do
+  describe ".get_forecast" do
+    let(:args) { { lat: 37.3293, lon: -121.891 } }
+    
+    # Stubbed JSON strings to work with String#as_hash
+    let(:location_response_body) do
+      '{"properties":{"gridId":"MTR","gridX":103,"gridY":81}}'
+    end
+    
+    let(:forecast_response_body) do
+      '{
+        "properties":{
+          "periods":[
+            {"name":"Today","temperature":75},
+            {"name":"Tonight","temperature":60}
+          ]
+        }
+      }'
+    end
+    
+    # Mock HTTParty responses
+    let(:location_response) { double("HTTParty::Response", body: location_response_body) }
+    let(:forecast_response) { double("HTTParty::Response", body: forecast_response_body) }
+    
+    before do
+      # Stub the NationalWeatherService calls
+      allow(Weather::NationalWeatherService).to receive(:send_request)
+                                                  .with(Weather::NationalWeatherService::Endpoints::GET_LOCATION_DATA, args)
+                                                  .and_return(location_response)
+      
+      allow(Weather::NationalWeatherService).to receive(:send_request)
+                                                  .with(
+                                                    Weather::NationalWeatherService::Endpoints::GET_FORECAST,
+                                                    { grid_id: "MTR", x: 103, y: 81 }
+                                                  )
+                                                  .and_return(forecast_response)
+    end
+    
+    it "calls NationalWeatherService with correct endpoints and arguments" do
+      Weather::WeatherService.get_forecast(args)
+      
+      expect(Weather::NationalWeatherService).to have_received(:send_request)
+                                                   .with(Weather::NationalWeatherService::Endpoints::GET_LOCATION_DATA, args)
+      
+      expect(Weather::NationalWeatherService).to have_received(:send_request)
+                                                   .with(
+                                                     Weather::NationalWeatherService::Endpoints::GET_FORECAST,
+                                                     { grid_id: "MTR", x: 103, y: 81 }
+                                                   )
+    end
+    
+    it "returns the periods array from the forecast response" do
+      result = Weather::WeatherService.get_forecast(args)
+      
+      expect(result).to eq([
+                             { "name" => "Today", "temperature" => 75 },
+                             { "name" => "Tonight", "temperature" => 60 }
+                           ])
+    end
+  end
+end
